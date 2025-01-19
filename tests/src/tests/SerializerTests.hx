7164c45d77418afdc3a18e4b221139ab28cbc683
Ne_Eo <NeeEoo@users.noreply.github.com>
2025-01-19 03:34:40 +0100

    Small change

--- tests/src/tests/SerializerTests.hx
+++ tests/src/tests/SerializerTests.hx
@@ -113,10 +113,10 @@ class SerializerTests {
 		// });
 
 		AssertUtils.shouldFinish("Writes STRING_8", function() {
-			assert(serialize("ABC"), "0a 03 41 42 43");
-			assert(serialize("ABCD"), "0a 04 41 42 43 44");
-			assert(serialize("ABCDE"), "0a 05 41 42 43 44 45");
-			assert(serialize(""), "0a 00");
+			assert(serialize("ABC"), "0e 03 41 42 43");
+			assert(serialize("ABCD"), "0e 04 41 42 43 44");
+			assert(serialize("ABCDE"), "0e 05 41 42 43 44 45");
+			assert(serialize(""), "0e 00");
 		});
 
 		AssertUtils.shouldFinish("Writes STRING_16", function() {
@@ -129,7 +129,7 @@ class SerializerTests {
 			for (i in 0...1000) {
 				abcHex += "414243";
 			}
-			assert(serialize(abc), "0b bb 0b " + abcHex);
+			assert(serialize(abc), "0f bb 0b " + abcHex);
 		});
 
 		// Too big to test
@@ -141,86 +141,86 @@ class SerializerTests {
 		});*/
 
 		AssertUtils.shouldFinish("Writes NAN", function() {
-			assert(serialize(Math.NaN), "0d");
+			assert(serialize(Math.NaN), "0a");
 		});
 
 		AssertUtils.shouldFinish("Writes POSITIVE_INFINITY", function() {
-			assert(serialize(Math.POSITIVE_INFINITY), "0e");
+			assert(serialize(Math.POSITIVE_INFINITY), "0b");
 		});
 
 		AssertUtils.shouldFinish("Writes NEGATIVE_INFINITY", function() {
-			assert(serialize(Math.NEGATIVE_INFINITY), "0f");
+			assert(serialize(Math.NEGATIVE_INFINITY), "0c");
 		});
 
 		AssertUtils.shouldFinish("Writes ARRAY", function() {
-			assert(serialize([1, 2, 3]), "10 0401 0402 0403 ff");
-			assert(serialize([1, 2, 3, 4]), "10 0401 0402 0403 0404 ff");
+			assert(serialize([1, 2, 3]), "11 0401 0402 0403 ff");
+			assert(serialize([1, 2, 3, 4]), "11 0401 0402 0403 0404 ff");
 		});
 
 		AssertUtils.shouldFinish("Writes OBJECT", function() {
-			assert(serialize({a: 1}), "11 0a0161: 0401 ff");
-			assert(serialize({a: 1, b: 2}), "11 0a0161: 0401 | 0a0162: 0402 ff");
-			assert(serialize({a: 1, b: 2, c: 3}), "11 0a0161: 0401 | 0a0162: 0402 | 0a0163: 0403 ff");
+			assert(serialize({a: 1}), "12 0e0161: 0401 ff");
+			assert(serialize({a: 1, b: 2}), "12 0e0161: 0401 | 0e0162: 0402 ff");
+			assert(serialize({a: 1, b: 2, c: 3}), "12 0e0161: 0401 | 0e0162: 0402 | 0e0163: 0403 ff");
 		});
 
 		AssertUtils.shouldFinish("Writes OBJECT_REF_8", function() {
 			var obj = {a: null};
 			Serializer.USE_CACHE = true;
-			assert(serialize([obj, obj]), "10! {11 0a0161: 00 ff} 12 01 !ff");
-			assert(serialize([obj, obj, obj]), "10! {11 0a0161: 00 ff} 12 01 12 01 !ff");
+			assert(serialize([obj, obj]), "11! {12 0e0161: 00 ff} 13 01 !ff");
+			assert(serialize([obj, obj, obj]), "11! {12 0e0161: 00 ff} 13 01 13 01 !ff");
 			Serializer.USE_CACHE = false;
 		});
 
 		/*AssertUtils.shouldFinish("Writes OBJECT_REF_16", function() {
 				var obj = {a: null};
-				assert(serialize([obj, obj]), "10! {11 0a0161: 00 ff} 13 0100 !ff");
-				assert(serialize([obj, obj, obj]), "10! {11 0a0161: 00 ff} 13 0100 13 0100 !ff");
+				assert(serialize([obj, obj]), "11! {12 0e0161: 00 ff} 14 0100 !ff");
+				assert(serialize([obj, obj, obj]), "11! {12 0e0161: 00 ff} 14 0100 14 0100 !ff");
 			});
 
 			AssertUtils.shouldFinish("Writes OBJECT_REF_32", function() {
 				var obj = {a: null};
-				assert(serialize([obj, obj]), "10! {11 0a0161: 00 ff} 14 01000000 !ff");
-				assert(serialize([obj, obj, obj]), "10! {11 0a0161: 00 ff} 14 01000000 14 01000000 !ff");
+				assert(serialize([obj, obj]), "11! {12 0e0161: 00 ff} 15 01000000 !ff");
+				assert(serialize([obj, obj, obj]), "11! {12 0e0161: 00 ff} 15 01000000 15 01000000 !ff");
 		});*/
 
 		AssertUtils.shouldFinish("Writes STRING_REF_8", function() {
-			assert(serialize(["ABC", "ABC"]), "10! 0a03_414243 15 00 !ff");
-			assert(serialize(["ABC", "ABC", "ABC"]), "10! 0a03_414243 15 00 15 00 !ff");
+			assert(serialize(["ABC", "ABC"]), "11! 0e03_414243 16 00 !ff");
+			assert(serialize(["ABC", "ABC", "ABC"]), "11! 0e03_414243 16 00 16 00 !ff");
 		});
 
 		/*AssertUtils.shouldFinish("Writes STRING_REF_16", function() {
-				assert(serialize(["ABC", "ABC"]), "10! 0a03_414243 16 0000 !ff");
-				assert(serialize(["ABC", "ABC", "ABC"]), "10! 0a03_414243 16 0000 16 0000 !ff");
+				assert(serialize(["ABC", "ABC"]), "11! 0e03_414243 17 0000 !ff");
+				assert(serialize(["ABC", "ABC", "ABC"]), "11! 0e03_414243 17 0000 17 0000 !ff");
 			});
 
 			AssertUtils.shouldFinish("Writes STRING_REF_32", function() {
-				assert(serialize(["ABC", "ABC"]), "10! 0a03_414243 17 00000000 !ff");
-				assert(serialize(["ABC", "ABC", "ABC"]), "10! 0a03_414243 17 00000000 17 00000000 !ff");
+				assert(serialize(["ABC", "ABC"]), "11! 0e03_414243 18 00000000 !ff");
+				assert(serialize(["ABC", "ABC", "ABC"]), "11! 0e03_414243 18 00000000 18 00000000 !ff");
 		});*/
 
 		AssertUtils.shouldFinish("Writes EXCEPTION", function() {
 			var serializer = new Serializer();
 			serializer.serializeException("ABC");
-			assert(serializer.toString(), "18 0a03_414243");
+			assert(serializer.toString(), "19 0e03_414243");
 		});
 
 		AssertUtils.shouldFinish("Writes CLASS_INSTANCE", function() {
 			//                        "tests.TestClass" == 74657374732e54657374436c617373
-			assert(serialize(new TestClass(0, "ABC")), "19 0a 0f 74657374732e54657374436c617373 {0a0161=03 0a0162=0a03414243 ff}");
-			assert(serialize(new TestClass(1, "A")), "19 0a 0f 74657374732e54657374436c617373 {0a0161=0401 0a0162=0a0141 ff}");
+			assert(serialize(new TestClass(0, "ABC")), "1a 0e 0f 74657374732e54657374436c617373 {0e0161=03 0e0162=0e03414243 ff}");
+			assert(serialize(new TestClass(1, "A")), "1a 0e 0f 74657374732e54657374436c617373 {0e0161=0401 0e0162=0e0141 ff}");
 		});
 
 		AssertUtils.shouldFinish("Writes ENUM_INSTANCE", function() {
 			//                         "tests.TestEnum" == 74657374732e54657374456e756d
-			assert(serialize(TestEnum.A), "1a 0a 0e 74657374732e54657374456e756d 0a0141 [ff]");
-			assert(serialize(TestEnum.B(1)), "1a 0a 0e 74657374732e54657374456e756d 0a0142 [0401 ff]");
+			assert(serialize(TestEnum.A), "1b 0e 0e 74657374732e54657374456e756d 0e0141 [ff]");
+			assert(serialize(TestEnum.B(1)), "1b 0e 0e 74657374732e54657374456e756d 0e0142 [0401 ff]");
 		});
 
 		AssertUtils.shouldFinish("Writes LIST", function() {
 			var list = new haxe.ds.List();
 			list.add("ABC");
 			list.add("DEF");
-			assert(serialize(list), "1b 0a03 414243 0a03 444546 ff");
+			assert(serialize(list), "1c 0e03 414243 0e03 444546 ff");
 		});
 
 		AssertUtils.shouldFinish("Writes STRING_MAP", function() {
@@ -228,8 +228,8 @@ class SerializerTests {
 			map.set("ABC", "DEF");
 			map.set("GHI", "JKL");
 			assertAny(serialize(map), [
-				"1c {0a03 414243:0a03 444546} {0a03 474849:0a03 4a4b4c} ff",
-				"1c {0a03 474849:0a03 4a4b4c} {0a03 414243:0a03 444546} ff"
+				"1d {0e03 414243:0e03 444546} {0e03 474849:0e03 4a4b4c} ff",
+				"1d {0e03 474849:0e03 4a4b4c} {0e03 414243:0e03 444546} ff"
 			]);
 		});
 
@@ -237,7 +237,7 @@ class SerializerTests {
 			var map = new haxe.ds.IntMap();
 			map.set(1, 2);
 			map.set(3, 4);
-			assertAny(serialize(map), ["1d {0401:0402} {0403:0404} ff", "1d {0403:0404} {0401:0402} ff"]);
+			assertAny(serialize(map), ["1e {0401:0402} {0403:0404} ff", "1e {0403:0404} {0401:0402} ff"]);
 		});
 
 		AssertUtils.shouldFinish("Writes OBJECT_MAP", function() {
@@ -245,8 +245,8 @@ class SerializerTests {
 			map.set({a: 1}, {b: 2});
 			map.set({c: 3}, {d: 4});
 			assertAny(serialize(map), [
-				"1e {{11 0a0161:0401 ff}:{11 0a0162:0402 ff}} {{11 0a0163:0403 ff}:{11 0a0164:0404 ff}} ff",
-				"1e {{11 0a0163:0403 ff}:{11 0a0164:0404 ff}} {{11 0a0161:0401 ff}:{11 0a0162:0402 ff}} ff"
+				"1f {{12 0e0161:0401 ff}:{12 0e0162:0402 ff}} {{12 0e0163:0403 ff}:{12 0e0164:0404 ff}} ff",
+				"1f {{12 0e0163:0403 ff}:{12 0e0164:0404 ff}} {{12 0e0161:0401 ff}:{12 0e0162:0402 ff}} ff"
 			]);
 		});
 
@@ -254,53 +254,53 @@ class SerializerTests {
 			var map = new haxe.ds.EnumValueMap<TestEnum, Dynamic>();
 			map.set(TestEnum.A, {a: 1});
 			map.set(TestEnum.B(1), {b: 2});
-			var eStart = "1a 0a0e 74657374732e54657374456e756d";
-			var eSecond = "1a 15 00";
+			var eStart = "1b 0e0e 74657374732e54657374456e756d";
+			var eSecond = "1b 15 00";
 
-			var eAHex = "0a0141 [ff]";
-			var eB_1_Hex = "0a0142 [0401 ff]";
+			var eAHex = "0e0141 [ff]";
+			var eB_1_Hex = "0e0142 [0401 ff]";
 
 			// @formatter:off
 			assertAny(serialize(map), [
-                "1f [" + eStart + eAHex + " 11 0a0161:0401 ff] [" + eSecond + eB_1_Hex + " 11 0a0162:0402 ff] ff",
-                "1f [" + eStart + eB_1_Hex + " 11 0a0162:0402 ff] [" + eSecond + eAHex + " 11 0a0161:0401 ff] ff",
+                "20 [" + eStart + eAHex + " 12 0e0161:0401 ff] [" + eSecond + eB_1_Hex + " 12 0e0162:0402 ff] ff",
+                "20 [" + eStart + eB_1_Hex + " 12 0e0162:0402 ff] [" + eSecond + eAHex + " 12 0e0161:0401 ff] ff",
 			]);
 			// @formatter:on
 		});
 
 		AssertUtils.shouldFinish("Writes DATE", function() {
-			assert(serialize(Date.fromString("1988-12-25")), "20 0000b053f46e6142");
-			assert(serialize(Date.fromString("2005-10-22")), "20 0000b02053717042");
+			assert(serialize(Date.fromString("1988-12-25")), "21 0000b053f46e6142");
+			assert(serialize(Date.fromString("2005-10-22")), "21 0000b02053717042");
 		});
 
 		AssertUtils.shouldFinish("Writes BYTES", function() {
-			assert(serialize(haxe.io.Bytes.ofHex("414141")), "21 0403 414141");
-			assert(serialize(haxe.io.Bytes.ofHex("00ff7f")), "21 0403 00ff7f");
+			assert(serialize(haxe.io.Bytes.ofHex("414141")), "22 0403 414141");
+			assert(serialize(haxe.io.Bytes.ofHex("00ff7f")), "22 0403 00ff7f");
 		});
 
 		AssertUtils.shouldFinish("Writes CLASS_TYPE", function() {
 			// "tests.TestClass" == 74657374732e54657374436c617373
 			var testClassHex = "74657374732e54657374436c617373";
-			assert(serialize(TestClass), "22 0a0f " + testClassHex);
+			assert(serialize(TestClass), "23 0e0f " + testClassHex);
 		});
 
 		AssertUtils.shouldFinish("Writes ENUM_TYPE", function() {
 			// "tests.TestEnum" == 74657374732e54657374456e756d
 			var testEnumHex = "74657374732e54657374456e756d";
-			assert(serialize(TestEnum), "23 0a0e " + testEnumHex);
+			assert(serialize(TestEnum), "24 0e0e " + testEnumHex);
 		});
 
 		AssertUtils.shouldFinish("Writes NEG_ONE", function() {
-			assert(serialize(-1), "24");
+			assert(serialize(-1), "25");
 		});
 
 		AssertUtils.shouldFinish("Writes EMPTY_SPACE", function() {
-			assert(serialize([1, 2, 3, 4, 5]), "10 0401 0402 0403 0404 0405 ff");
-			assert(serialize([1, null, null, null, 5]), "10 0401 fc 03 0405 ff");
+			assert(serialize([1, 2, 3, 4, 5]), "11 0401 0402 0403 0404 0405 ff");
+			assert(serialize([1, null, null, null, 5]), "11 0401 fc 03 0405 ff");
 		});
 
 		AssertUtils.shouldFinish("Writes PI", function() {
-			assert(serialize(Math.PI), "25");
+			assert(serialize(Math.PI), "0d");
 		});
 
 		AssertUtils.shouldFinish("Writes NEG_INT8", function() {
--- tests/src/tests/UnserializerTests.hx
+++ tests/src/tests/UnserializerTests.hx
@@ -108,177 +108,177 @@ class UnserializerTests {
 		});
 
 		AssertUtils.shouldFinish("Parse STRING_8", function() {
-			AssertUtils.assertEquals(unserialize("0a 03 41 42 43"), "ABC");
-			AssertUtils.assertEquals(unserialize("0a 04 41 42 43 44"), "ABCD");
-			AssertUtils.assertEquals(unserialize("0a 05 41 42 43 44 45"), "ABCDE");
-			AssertUtils.assertEquals(unserialize("0a 00"), "");
+			AssertUtils.assertEquals(unserialize("0e 03 41 42 43"), "ABC");
+			AssertUtils.assertEquals(unserialize("0e 04 41 42 43 44"), "ABCD");
+			AssertUtils.assertEquals(unserialize("0e 05 41 42 43 44 45"), "ABCDE");
+			AssertUtils.assertEquals(unserialize("0e 00"), "");
 		});
 
 		AssertUtils.shouldFinish("Parse STRING_16", function() {
-			AssertUtils.assertEquals(unserialize("0b 03 00 41 42 43"), "ABC");
-			AssertUtils.assertEquals(unserialize("0b 04 00 41 42 43 44"), "ABCD");
-			AssertUtils.assertEquals(unserialize("0b 05 00 41 42 43 44 45"), "ABCDE");
-			AssertUtils.assertEquals(unserialize("0b 00 00"), "");
+			AssertUtils.assertEquals(unserialize("0f 03 00 41 42 43"), "ABC");
+			AssertUtils.assertEquals(unserialize("0f 04 00 41 42 43 44"), "ABCD");
+			AssertUtils.assertEquals(unserialize("0f 05 00 41 42 43 44 45"), "ABCDE");
+			AssertUtils.assertEquals(unserialize("0f 00 00"), "");
 		});
 
 		AssertUtils.shouldFinish("Parse STRING_32", function() {
-			AssertUtils.assertEquals(unserialize("0c 03 00 00 00 41 42 43"), "ABC");
-			AssertUtils.assertEquals(unserialize("0c 04 00 00 00 41 42 43 44"), "ABCD");
-			AssertUtils.assertEquals(unserialize("0c 05 00 00 00 41 42 43 44 45"), "ABCDE");
-			AssertUtils.assertEquals(unserialize("0c 00 00 00 00"), "");
+			AssertUtils.assertEquals(unserialize("10 03 00 00 00 41 42 43"), "ABC");
+			AssertUtils.assertEquals(unserialize("10 04 00 00 00 41 42 43 44"), "ABCD");
+			AssertUtils.assertEquals(unserialize("10 05 00 00 00 41 42 43 44 45"), "ABCDE");
+			AssertUtils.assertEquals(unserialize("10 00 00 00 00"), "");
 		});
 
 		AssertUtils.shouldFinish("Parse NAN", function() {
-			AssertUtils.assertEquals(unserialize("0d"), Math.NaN);
+			AssertUtils.assertEquals(unserialize("0a"), Math.NaN);
 		});
 
 		AssertUtils.shouldFinish("Parse POSITIVE_INFINITY", function() {
-			AssertUtils.assertEquals(unserialize("0e"), Math.POSITIVE_INFINITY);
+			AssertUtils.assertEquals(unserialize("0b"), Math.POSITIVE_INFINITY);
 		});
 
 		AssertUtils.shouldFinish("Parse NEGATIVE_INFINITY", function() {
-			AssertUtils.assertEquals(unserialize("0f"), Math.NEGATIVE_INFINITY);
+			AssertUtils.assertEquals(unserialize("0c"), Math.NEGATIVE_INFINITY);
 		});
 
 		AssertUtils.shouldFinish("Parse ARRAY", function() {
-			AssertUtils.assertEquals(unserialize("10 ff"), []);
-			AssertUtils.assertEquals(unserialize("10 00 ff"), [null]);
-			AssertUtils.assertEquals(unserialize("10 01 ff"), [true]);
-			AssertUtils.assertEquals(unserialize("10 02 ff"), [false]);
-			AssertUtils.assertEquals(unserialize("10 0a 03 41 42 43 ff"), ["ABC"]);
+			AssertUtils.assertEquals(unserialize("11 ff"), []);
+			AssertUtils.assertEquals(unserialize("11 00 ff"), [null]);
+			AssertUtils.assertEquals(unserialize("11 01 ff"), [true]);
+			AssertUtils.assertEquals(unserialize("11 02 ff"), [false]);
+			AssertUtils.assertEquals(unserialize("11 0e 03 41 42 43 ff"), ["ABC"]);
 		});
 
 		AssertUtils.shouldFinish("Parse OBJECT", function() {
-			AssertUtils.assertEquals(unserialize("{11 0a0161: 00 ff}"), {a: null});
-			AssertUtils.assertEquals(unserialize("{11 0a0161: {11 0a0162: 04ff ff} ff}"), {a: {b: 255}});
+			AssertUtils.assertEquals(unserialize("{12 0e0161: 00 ff}"), {a: null});
+			AssertUtils.assertEquals(unserialize("{12 0e0161: {12 0e0162: 04ff ff} ff}"), {a: {b: 255}});
 		});
 
 		AssertUtils.shouldFinish("Parse OBJECT_REF_8", function() {
 			var obj = {a: null};
-			AssertUtils.assertEquals(unserialize("[10! {11 0a0161: 00 ff} 12 01 !ff]"), [obj, obj]);
-			AssertUtils.assertEquals(unserialize("[10! {11 0a0161: 00 ff} 12 01 12 01 !ff]"), [obj, obj, obj]);
+			AssertUtils.assertEquals(unserialize("[11! {12 0e0161: 00 ff} 13 01 !ff]"), [obj, obj]);
+			AssertUtils.assertEquals(unserialize("[11! {12 0e0161: 00 ff} 13 01 13 01 !ff]"), [obj, obj, obj]);
 		});
 
 		AssertUtils.shouldFinish("Parse OBJECT_REF_16", function() {
 			var obj = {a: null};
-			AssertUtils.assertEquals(unserialize("[10! {11 0a0161: 00 ff} 13 0100 !ff]"), [obj, obj]);
-			AssertUtils.assertEquals(unserialize("[10! {11 0a0161: 00 ff} 13 0100 13 0100 !ff]"), [obj, obj, obj]);
+			AssertUtils.assertEquals(unserialize("[11! {12 0e0161: 00 ff} 14 0100 !ff]"), [obj, obj]);
+			AssertUtils.assertEquals(unserialize("[11! {12 0e0161: 00 ff} 14 0100 14 0100 !ff]"), [obj, obj, obj]);
 		});
 
 		AssertUtils.shouldFinish("Parse OBJECT_REF_32", function() {
 			var obj = {a: null};
-			AssertUtils.assertEquals(unserialize("[10! {11 0a0161: 00 ff} 14 01000000 !ff]"), [obj, obj]);
-			AssertUtils.assertEquals(unserialize("[10! {11 0a0161: 00 ff} 14 01000000 14 01000000 !ff]"), [obj, obj, obj]);
+			AssertUtils.assertEquals(unserialize("[11! {12 0e0161: 00 ff} 15 01000000 !ff]"), [obj, obj]);
+			AssertUtils.assertEquals(unserialize("[11! {12 0e0161: 00 ff} 15 01000000 15 01000000 !ff]"), [obj, obj, obj]);
 		});
 
 		AssertUtils.shouldFinish("Parse STRING_REF_8", function() {
-			AssertUtils.assertEquals(unserialize("[10! 0a03_414243 15 00 !ff]"), ["ABC", "ABC"]);
-			AssertUtils.assertEquals(unserialize("[10! 0a03_414243 15 00 15 00 !ff]"), ["ABC", "ABC", "ABC"]);
+			AssertUtils.assertEquals(unserialize("[11! 0e03_414243 16 00 !ff]"), ["ABC", "ABC"]);
+			AssertUtils.assertEquals(unserialize("[11! 0e03_414243 16 00 16 00 !ff]"), ["ABC", "ABC", "ABC"]);
 		});
 
 		AssertUtils.shouldFinish("Parse STRING_REF_16", function() {
-			AssertUtils.assertEquals(unserialize("[10! 0a03_414243 16 0000 !ff]"), ["ABC", "ABC"]);
-			AssertUtils.assertEquals(unserialize("[10! 0a03_414243 16 0000 16 0000 !ff]"), ["ABC", "ABC", "ABC"]);
+			AssertUtils.assertEquals(unserialize("[11! 0e03_414243 17 0000 !ff]"), ["ABC", "ABC"]);
+			AssertUtils.assertEquals(unserialize("[11! 0e03_414243 17 0000 17 0000 !ff]"), ["ABC", "ABC", "ABC"]);
 		});
 
 		AssertUtils.shouldFinish("Parse STRING_REF_32", function() {
-			AssertUtils.assertEquals(unserialize("[10! 0a03_414243 17 00000000 !ff]"), ["ABC", "ABC"]);
-			AssertUtils.assertEquals(unserialize("[10! 0a03_414243 17 00000000 17 00000000 !ff]"), ["ABC", "ABC", "ABC"]);
+			AssertUtils.assertEquals(unserialize("[11! 0e03_414243 18 00000000 !ff]"), ["ABC", "ABC"]);
+			AssertUtils.assertEquals(unserialize("[11! 0e03_414243 18 00000000 18 00000000 !ff]"), ["ABC", "ABC", "ABC"]);
 		});
 
 		AssertUtils.shouldFinish("Parse EXCEPTION", function() {
 			AssertUtils.assertThrows("ABC", function() {
-				unserialize("18 0a03_414243");
+				unserialize("19 0e03_414243");
 			});
 			AssertUtils.assertThrows({a: 1}, function() {
-				unserialize("18 {11 0a0161: 0401 ff}");
+				unserialize("19 {11 0e0161: 0401 ff}");
 			});
 		});
 
 		AssertUtils.shouldFinish("Parse CLASS_INSTANCE", function() {
 			//                        "tests.TestClass" == 74657374732e54657374436c617373
-			AssertUtils.assertEquals(unserialize("19 0a 0f 74657374732e54657374436c617373 {0a0161=03 0a0162=0a03414243 ff}").toString(),
+			AssertUtils.assertEquals(unserialize("1a 0e 0f 74657374732e54657374436c617373 {0e0161=03 0e0162=0e03414243 ff}").toString(),
 				(new TestClass(0, "ABC")).toString());
-			AssertUtils.assertEquals(unserialize("19 0a 0f 74657374732e54657374436c617373 {0a0161=0401 0a0162=0a0141 ff}").toString(),
+			AssertUtils.assertEquals(unserialize("1a 0e 0f 74657374732e54657374436c617373 {0e0161=0401 0e0162=0e0141 ff}").toString(),
 				(new TestClass(1, "A")).toString());
 		});
 
 		AssertUtils.shouldFinish("Parse ENUM_INSTANCE", function() {
 			//                         "tests.TestEnum" == 74657374732e54657374456e756d
-			AssertUtils.assertEquals(unserialize("1a 0a 0e 74657374732e54657374456e756d 0a0141 [ff]"), TestEnum.A);
-			AssertUtils.assertEquals(unserialize("1a 0a 0e 74657374732e54657374456e756d 0a0142 [0401 ff]"), TestEnum.B(1));
+			AssertUtils.assertEquals(unserialize("1b 0e 0e 74657374732e54657374456e756d 0e0141 [ff]"), TestEnum.A);
+			AssertUtils.assertEquals(unserialize("1b 0e 0e 74657374732e54657374456e756d 0e0142 [0401 ff]"), TestEnum.B(1));
 		});
 
 		AssertUtils.shouldFinish("Parse LIST", function() {
 			var list = new haxe.ds.List();
 			list.add("ABC");
 			list.add("DEF");
-			AssertUtils.assertEquals(unserialize("1b 0a03 414243 0a03 444546 ff"), list);
+			AssertUtils.assertEquals(unserialize("1c 0e03 414243 0e03 444546 ff"), list);
 		});
 
 		AssertUtils.shouldFinish("Parse STRING_MAP", function() {
 			var map = new haxe.ds.StringMap();
 			map.set("ABC", "DEF");
 			map.set("GHI", "JKL");
-			AssertUtils.assertEquals(unserialize("1c {0a03 414243:0a03 444546} {0a03 474849:0a03 4a4b4c} ff"), map);
+			AssertUtils.assertEquals(unserialize("1d {0e03 414243:0e03 444546} {0e03 474849:0e03 4a4b4c} ff"), map);
 		});
 
 		AssertUtils.shouldFinish("Parse INT_MAP", function() {
 			var map = new haxe.ds.IntMap();
 			map.set(1, 2);
 			map.set(3, 4);
-			AssertUtils.assertEquals(unserialize("1d {0401:0402} {0403:0404} ff"), map);
+			AssertUtils.assertEquals(unserialize("1e {0401:0402} {0403:0404} ff"), map);
 		});
 
 		AssertUtils.shouldFinish("Parse OBJECT_MAP", function() {
 			var map = new haxe.ds.ObjectMap<Dynamic, Dynamic>();
 			map.set({a: 1}, {b: 2});
 			map.set({c: 3}, {d: 4});
-			AssertUtils.assertEquals(unserialize("1e {{11 0a0161:0401 ff}:{11 0a0162:0402 ff}} {{11 0a0163:0403 ff}:{11 0a0164:0404 ff}} ff"), map);
+			AssertUtils.assertEquals(unserialize("1f {{12 0e0161:0401 ff}:{12 0e0162:0402 ff}} {{12 0e0163:0403 ff}:{12 0e0164:0404 ff}} ff"), map);
 		});
 
 		AssertUtils.shouldFinish("Parse ENUM_MAP", function() {
 			var map = new haxe.ds.EnumValueMap<TestEnum, Dynamic>();
 			map.set(TestEnum.A, {a: 1});
 			map.set(TestEnum.B(1), {b: 2});
-			var testEnumAHex = "1a 0a0e 74657374732e54657374456e756d 0a0141 [ff]";
-			var testEnumB_1_Hex = "1a 0a0e 74657374732e54657374456e756d 0a0142 [0401 ff]";
-			AssertUtils.assertEquals(unserialize("1f [" + testEnumAHex + " 11 0a0161:0401 ff] [" + testEnumB_1_Hex + " 11 0a0162:0402 ff] ff"), map);
+			var testEnumAHex = "1b 0e0e 74657374732e54657374456e756d 0e0141 [ff]";
+			var testEnumB_1_Hex = "1b 0e0e 74657374732e54657374456e756d 0e0142 [0401 ff]";
+			AssertUtils.assertEquals(unserialize("20 [" + testEnumAHex + " 12 0e0161:0401 ff] [" + testEnumB_1_Hex + " 12 0e0162:0402 ff] ff"), map);
 		});
 
 		AssertUtils.shouldFinish("Parse DATE", function() {
-			AssertUtils.assertEquals(unserialize("20 0000b053f46e6142"), Date.fromString("1988-12-25")); // random date
-			AssertUtils.assertEquals(unserialize("20 0000b02053717042"), Date.fromString("2005-10-22")); // when haxe started
+			AssertUtils.assertEquals(unserialize("21 0000b053f46e6142"), Date.fromString("1988-12-25")); // random date
+			AssertUtils.assertEquals(unserialize("21 0000b02053717042"), Date.fromString("2005-10-22")); // when haxe started
 		});
 
 		AssertUtils.shouldFinish("Parse BYTES", function() {
-			AssertUtils.assertEquals(unserialize("21 0403 414141"), haxe.io.Bytes.ofHex("414141"));
-			AssertUtils.assertEquals(unserialize("21 0403 00ff7f"), haxe.io.Bytes.ofHex("00ff7f"));
+			AssertUtils.assertEquals(unserialize("22 0403 414141"), haxe.io.Bytes.ofHex("414141"));
+			AssertUtils.assertEquals(unserialize("22 0403 00ff7f"), haxe.io.Bytes.ofHex("00ff7f"));
 		});
 
 		AssertUtils.shouldFinish("Parse CLASS_TYPE", function() {
 			// "tests.TestClass" == 74657374732e54657374436c617373
 			var testClassHex = "74657374732e54657374436c617373";
-			AssertUtils.assertEquals(unserialize("22 0a0f " + testClassHex), TestClass);
+			AssertUtils.assertEquals(unserialize("23 0e0f " + testClassHex), TestClass);
 		});
 
 		AssertUtils.shouldFinish("Parse ENUM_TYPE", function() {
 			// "tests.TestEnum" == 74657374732e54657374456e756d
 			var testEnumHex = "74657374732e54657374456e756d";
-			AssertUtils.assertEquals(unserialize("23 0a0e " + testEnumHex), TestEnum);
+			AssertUtils.assertEquals(unserialize("24 0e0e " + testEnumHex), TestEnum);
 		});
 
 		AssertUtils.shouldFinish("Parse NEG_ONE", function() {
-			AssertUtils.assertEquals(unserialize("24"), -1);
+			AssertUtils.assertEquals(unserialize("25"), -1);
 		});
 
 		AssertUtils.shouldFinish("Parse EMPTY_SPACE", function() {
-			AssertUtils.assertEquals(unserialize("10 0401 00 00 00 0405 ff"), [1, null, null, null, 5]);
-			AssertUtils.assertEquals(unserialize("10 0401 fc 03 0405 ff"), [1, null, null, null, 5]);
+			AssertUtils.assertEquals(unserialize("11 0401 00 00 00 0405 ff"), [1, null, null, null, 5]);
+			AssertUtils.assertEquals(unserialize("11 0401 fc 03 0405 ff"), [1, null, null, null, 5]);
 		});
 
 		AssertUtils.shouldFinish("Parse PI", function() {
-			AssertUtils.assertEquals(unserialize("25"), Math.PI);
+			AssertUtils.assertEquals(unserialize("0d"), Math.PI);
 		});
 
 		AssertUtils.shouldFinish("Parse NEG_INT8", function() {
@@ -293,9 +293,9 @@ class UnserializerTests {
 		// bytes.setInt64(0, Int64.make(0x80000000, 0x00000000));
 		// trace(bytes.toHex());
 
-		// var hex = "10 0a 03 414243 11 0a 03 414141 00 FF 01 0f 15 00 ff";
+		// var hex = "10 0e 03 414243 11 0e 03 414141 00 FF 01 0f 15 00 ff";
 		// trace(Unserializer.run(fromHex(hex)));
-		// var hex = "10 0a 03 414243 11 0b 0300 414141 00 FF 01 0f 15 00 ff";
+		// var hex = "10 0e 03 414243 11 0b 0300 414141 00 FF 01 0f 15 00 ff";
 		// trace(Unserializer.run(fromHex(hex)));
 	}
 }
